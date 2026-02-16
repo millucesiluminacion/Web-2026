d98b3e682204c3d8885432fdbb792ed2fc0f1fd2
millucesiluminacion <milluces2018@gmail.com>
2026-02-16 16:55:27 +0100

    Footer Info

--- src/components/layout/MainFooterLinks.jsx
+++ src/components/layout/MainFooterLinks.jsx
@@ -1,4 +1,4 @@
-import { MapPin, Phone, Mail } from 'lucide-react';
+import { MapPin, Phone, Mail, CalendarClock } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export function MainFooterLinks() {
@@ -10,21 +10,28 @@ export function MainFooterLinks() {
                 <p className="mb-4 text-xs leading-relaxed">
                     Líderes en distribución de iluminación LED profesional y doméstica. Calidad certificada y los mejores precios del mercado.
                 </p>
-                <ul className="space-y-3 text-sm">
+                <ul className="space-y-3 text-xs">
                     <li className="flex gap-3 items-start">
                         <MapPin className="w-5 h-5 text-blue-500 shrink-0" />
-                        <span>Polígono Industrial Los Olivos,<br />C/ Principal 12, 28900 Getafe, Madrid</span>
+                        <span>Polígono Industrial Cobo Calleja,<br />C/ Rio Tormes 5, 28943 Fuenlabrada, Madrid</span>
+                    </li>
+                    <li className="flex gap-3 items-center">
+                        <CalendarClock className="w-5 h-5 text-blue-500 shrink-0" />
+                        <div className="flex flex-col">
+                            <span className="text-white font-bold">De Domingo a Viernes: 09:30 - 19:00</span>
+                            <span className="text-xs">Sabado: Cerrado por descanso</span>
+                        </div>
                     </li>
                     <li className="flex gap-3 items-center">
                         <Phone className="w-5 h-5 text-blue-500 shrink-0" />
                         <div className="flex flex-col">
-                            <span className="text-white font-bold hover:text-blue-400 cursor-pointer">+34 900 123 456</span>
-                            <span className="text-xs">L-V: 08:30 - 18:30</span>
+                            <a href="https://wa.me/+34689935436" className="text-white font-bold hover:text-blue-400 cursor-pointer">+34 689 935 436</a>
+                            <a href="https://callto:+34917654062" className="text-white font-bold hover:text-blue-400 cursor-pointer">+34 917 654 062</a>
                         </div>
                     </li>
                     <li className="flex gap-3 items-center">
                         <Mail className="w-5 h-5 text-blue-500 shrink-0" />
-                        <a href="mailto:info@milluces.com" className="hover:text-blue-400">info@milluces.com</a>
+                        <a href="mailto:millucesiluminacion@hotmail.com" className="hover:text-blue-400">millucesiluminacion@hotmail.com</a>
                     </li>
                 </ul>
             </div>
